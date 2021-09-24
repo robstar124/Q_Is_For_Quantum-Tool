38761522e01fd8fb177874cf1beeeb515581b957
Jabeth <J.Musumba@sms.ed.ac.uk>
2021-09-24 13:26:43 +0100

    QOL changes for Archimedes.py

--- Archimedes.py
+++ Archimedes.py
@@ -3,23 +3,26 @@ import sys
 import numpy as np
 import gates
 
-np.set_printoptions(threshold=sys.maxsize)
+
 
 # 4th bit is the target
 # 5th bit is the dummy
 class Archimedes:
-    def __init__(self, initState):
-
+    def __init__(self, initState, locations, fake=False, classic=False):
         self.matrix = initState
+        if fake:
+            self.locations = ["11110", "11110", "11110", "11110"]
+        else:
+            self.locations = locations  # "real" locations for the gold bars
 
-        self.matrix = np.matmul(gates.hadamard(5, [1, 2, 3, 4]), self.matrix)   # initial 4 hadamards
-        self.locations = ["00110", "01010", "10110", "11110"]  # "real" locations for the gold bars
-        self.buildCircuit()
-
-        self.matrix = np.matmul(gates.hadamard(5, [1, 2, 3, 4]), self.matrix)
+        if classic:
+            self.buildCircuit()
+        else:
+            self.matrix = np.matmul(gates.hadamard(5, [1, 2, 3, 4]), self.matrix)  # initial 4 hadamards
+            self.buildCircuit()
+            self.matrix = np.matmul(gates.hadamard(5, [1, 2, 3, 4]), self.matrix)
 
         self.runCircuit()
-
         pass
 
     def buildCircuit(
@@ -30,7 +33,7 @@ class Archimedes:
             for j in range(0, len(i) - 2):
                 if i[j] == "0":
                     if circNots[j] % 2 == 0:
-                        targets.append(j+1)
+                        targets.append(j + 1)
                         circNots[j] += 1
                     elif circNots[j] % 2 == 1:
                         continue
@@ -38,14 +41,14 @@ class Archimedes:
                     if circNots[j] % 2 == 0:
                         continue
                     elif circNots[j] % 2 == 1:
-                        targets.append(j+1)
+                        targets.append(j + 1)
                         circNots[j] += 1
 
             xGate = gates.NOT(5, targets)
             cccnot = gates.CCCNOT(1, 2, 3, 4, 5)
 
             self.matrix = np.matmul(cccnot, np.matmul(xGate, self.matrix))
-        return (self.matrix)
+        return self.matrix
 
     def runCircuit(self):
 
@@ -54,7 +57,8 @@ class Archimedes:
 
 def main():
     initState = gates.state_vector([0, 0, 1, 1, 0])
-    Archimedes(initState)
+    locations = ["01110", "01010", "10110", "11110"]
+    Archimedes(initState, locations)
 
 
 main()
